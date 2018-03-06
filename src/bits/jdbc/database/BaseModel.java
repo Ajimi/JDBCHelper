a17ca54fa57b6c2219840d22499ac7908182369a
Selim Ajimi <sloumma.21@gmail.com>
2018-03-06 14:53:56 +0100

    Add BaseModel and Update Append of ContentValues

--- src/bits/jdbc/database/BaseModel.java
+++ src/bits/jdbc/database/BaseModel.java
@@ -1,4 +1,18 @@
 package bits.jdbc.database;
 
-public class BaseModel {
+import bits.jdbc.content.ContentValues;
+
+import java.sql.ResultSet;
+
+public abstract class BaseModel {
+    public BaseModel() {
+    }
+
+    public BaseModel(ResultSet resultSet) {
+        resolve(resultSet);
+    }
+
+    public abstract ContentValues assemble();
+
+    public abstract void resolve(ResultSet resultSet);
 }
