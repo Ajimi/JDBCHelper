c2bc42c59793c5737db490f646d59194436bfdf4
Selim Ajimi <sloumma.21@gmail.com>
2018-03-06 16:56:40 +0100

    Update SQL CONNECTION Pool to accept password and username

--- src/bits/jdbc/Helper/SQLConnectionPool.java
+++ src/bits/jdbc/Helper/SQLConnectionPool.java
@@ -5,9 +5,18 @@ public class SQLConnectionPool extends ConnectionPool {
     private static final String CONNECTION_URL_BASE = "jdbc:mysql://localhost:3306/%s";
     private static final int DEFAULT_CONNECTION_NUM = 5;
 
-    //jdbc:mysql://localhost:3306/russia
-
     public SQLConnectionPool(String name) {
-        super(DEFAULT_CONNECTION_NUM, DRIVER_NAME, String.format(CONNECTION_URL_BASE, name), "", "");
+
+        this(name, "root", "");
     }
+
+    public SQLConnectionPool(String name, String username) {
+        this(name, username, "");
+    }
+
+    public SQLConnectionPool(String name, String username, String password) {
+        super(DEFAULT_CONNECTION_NUM, DRIVER_NAME, String.format(CONNECTION_URL_BASE, name), username, password);
+    }
+
+
 }
